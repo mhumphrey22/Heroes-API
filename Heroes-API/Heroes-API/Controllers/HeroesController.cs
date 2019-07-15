b287061e458f60d4ec709739093f9d3d08f77a82
mhumphrey22 <m_humphrey22@hotmail.com>
2019-07-14 19:01:30 -0500

    Added Delete
    Need to test some different Route methods.  Right now multiple Posts won't be supported.

--- Heroes-API/Heroes-API/Controllers/HeroesController.cs
+++ Heroes-API/Heroes-API/Controllers/HeroesController.cs
@@ -16,28 +16,36 @@ namespace HeroesAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Hero>> GetHeroes()
         {
-            var db = new TourOfHeroesContext();
-
-            return db.Hero;
+            using (var db = new TourOfHeroesContext())
+            {
+                //  Return all Heroes
+                return db.Hero;
+            }
         }
 
         // GET api/heroes/5
         [HttpGet("{id}")]
+        //[Route("api/heroes/getHero/{id}")]
         public ActionResult<Hero> GetHero(int id)
         {
-            var db = new TourOfHeroesContext();
-
-            return db.Hero
-                     .Where(x => x.Id == id)
-                     .FirstOrDefault();
+            using (var db = new TourOfHeroesContext())
+            {
+                //  Get Hero with Matching Id
+                return db.Hero
+                         .Where(x => x.Id == id)
+                         .FirstOrDefault();
+            }
         }
 
         // POST api/heroes
         [HttpPost]
-        public ActionResult<Hero> Post([FromBody] Hero hero)
+        [Route("api/heroes/saveHero")]
+        public ActionResult<Hero> SaveHero([FromBody] Hero hero)
         {
             using (var db = new TourOfHeroesContext())
             {
+                //  Determine state based off of Hero Id.  
+                //  Id = 0 is a new Hero.
                 db.Entry(hero).State = hero.Id == 0 ? EntityState.Added
                                                     : EntityState.Modified;
 
@@ -47,16 +55,22 @@ namespace HeroesAPI.Controllers
             }
         }
 
-        // PUT api/heroes/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
+        // DELETE api/values/5
         //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+        [HttpPost]
+        [Route("api/heroes/deleteHero")]
+        //public void DeleteHero(int id)
+        public void DeleteHero([FromBody] Hero hero)  
+        {
+            using (var db = new TourOfHeroesContext())
+            {
+                ////  
+                //var hero = db.Hero.Where(x => x.Id == id)
+                //                  .FirstOrDefault();
+
+                db.Hero.Remove(hero);
+            }
+
+        }
     }
 }
